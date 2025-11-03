572483184078aabe35712c0523431b0f4e70382c
RENATA RODRIGUES DA SILVA <renata.rsilva71@senacsp.edu.br>
2025-11-03 19:29:42 -0300

    feat: produto-form

---
+++ src/app/pages/produto-form/produto-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produto-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './produto-form.component.html',
+  styleUrl: './produto-form.component.css'
+})
+export class ProdutoFormComponent {
+
+}
