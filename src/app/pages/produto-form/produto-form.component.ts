a35fa7bf2d727e3406e06ef6e311b5e2ef1968ad
RENATA RODRIGUES DA SILVA <renata.rsilva71@senacsp.edu.br>
2025-11-03 19:48:50 -0300

    feat: produto-form

--- src/app/pages/produto-form/produto-form.component.ts
+++ src/app/pages/produto-form/produto-form.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button'
+import {MatInputModule} from '@angular/material/input'
+
 
 @Component({
   selector: 'app-produto-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatInputModule, MatButtonModule],
   templateUrl: './produto-form.component.html',
   styleUrl: './produto-form.component.css'
 })
 export class ProdutoFormComponent {
 
+    formulario = new FormGroup({
+      nome: new FormControl([Validators.required, 
+                             Validators.minLength(3)   
+      ]),
+      descricao: new FormControl([Validators.required, 
+                             Validators.minLength(3)   
+      ]),
+    });
+
+    //
+    onSumbmit(){
+    if(this.formulario.valid){
+      alert('Formulário enviado com sucesso! \n'+
+        JSON.stringify(this.formulario.value));
+      }
+    }
 }
